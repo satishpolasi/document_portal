e129ac01a5453f4730990a5f0e7eb5ed07a4ddea
Satish <satish.polasi@gmail.com>
2025-08-10 01:08:20 -0700

    data ingestion code is completed for multi document chat

--- src/multi_document_chat/data_ingestion.py
+++ src/multi_document_chat/data_ingestion.py
@@ -44,9 +44,64 @@ class DocumentIngestor:
             self.log.error("Failed to initialize DocumentIngestor ", error=str(e))
             raise DocumentPortalException("Initialization error in DocumentIngestor", sys)
     
-    def ingest_files(self):
-        pass
+    def ingest_files(self, uploaded_files):
+        try:
+            documents=[]
+            
+            for uploaded_file in uploaded_files:
+                ext = Path(uploaded_file.name).suffix.lower()
+                if ext not in self.SUPPORTED_EXTENSIONS:
+                    self.log.waring("Unsupported file skipped", filename=uploaded_file.name)
+                    continue
+                unique_filename = f"{uuid.uuid4().hex[:8]}{ext}"
+                temp_path = self.session_temp_dir / unique_filename
+                
+                with open(temp_path, "wb") as f:
+                    f.write(uploaded_file.read())
+                self.log.info("File saved for ingestion", filename=uploaded_file.name, saved_as=str(temp_path), session_id=self.session_id)
+                
+                if ext == ".pdf":
+                    loader = PyPDFLoader(str(temp_path))
+                elif ext == ".docx":
+                    loader = Docx2txtLoader(str(temp_path))
+                elif ext == ".txt":
+                    loader = TextLoader(str(temp_path), encoding="utf-8")
+                else:
+                    self.log.warning("Unsupported file type encountered", filename=uploaded_file.name)  
+                    continue
+                    
+                docs = loader.load()
+                documents.extend(docs)
+                
+            if not documents:
+                raise DocumentPortalException("No valid documents loaded", sys)
+                    
+            self.log.info("All documents loaded", total_docs=len(documents), session_id=self.session_id)
+            return self._create_retriever(documents)
+            
+        except Exception as e:
+            self.log.error("Failed to ingest files ", error=str(e))
+            raise DocumentPortalException("Ingestion error in DocumentIngestor", sys)
     
     def _create_retriever(self, documents):
-        pass
+        try:
+            splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
+            chunks = splitter.split_documents(documents)
+            self.log.info("Documents split into chunks", total_chunks=len(chunks), session_id=self.session_id)
+            
+            embeddings = self.model_loader.load_embeddings()
+            vectorstore = FAISS.from_documents(documents=chunks, embedding=embeddings)
+            
+            # Save FAISS index under session folder
+            vectorstore.save_local(str(self.session_faiss_dir))
+            self.log.info("FAISS index saved to disk", path=str(self.session_faiss_dir), session_id=self.session_id)
+            
+            retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+            
+            self.log.info("FAISS retriever created and ready to use", session_id=self.session_id)
+            return retriever
+            
+        except Exception as e:
+            self.log.error( "Failed to create retriever ", error=str(e))
+            raise DocumentPortalException("Retriever creation error in DocumentIngestor", sys)
     
