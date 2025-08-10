43753b8ee88722a70ff5f2131b904839478bd4b0
Satish <satish.polasi@gmail.com>
2025-08-09 19:41:15 -0700

    updated data ingestion for multi document chat

--- src/multi_document_chat/data_ingestion.py
+++ src/multi_document_chat/data_ingestion.py
@@ -1,12 +1,52 @@
+import uuid
+from pathlib import Path
+import sys
+from datetime import datetime, timezone
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from utils.model_loader import ModelLoader
+
 class DocumentIngestor:
     
-    def __init__(self):
-        pass
+    SUPPORTED_EXTENSIONS = {'.pdf', '.docx', '.txt', '.md'} 
+    
+    def __init__(self, temp_dir:str = "data/multi_document_chat", faiss_dir: str = "faiss_index", session_id: str | None = None):
+        try:
+            self.log = CustomLogger().get_logger(__name__)
+            
+            # base dirs
+            self.temp_dir = Path(temp_dir)
+            self.temp_dir.mkdir(parents=True, exist_ok=True)
+            self.faiss_dir = Path(faiss_dir)
+            self.faiss_dir.mkdir(parents=True, exist_ok=True)
+            
+            # sessionized paths
+            self.session_id = session_id or f"session_{datetime.now(timezone.utc).strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}
+            self.session_temp_dir = self.temp_dir / self.session_id
+            self.session_faiss_dir = self.faiss_dir / self.session_id
+            self.session_temp_dir.mkdir(parents=True, exist_ok=True)
+            self.session_faiss_dir.mkdir(parents=True, exist_ok=True)
+            
+            self.model_loader = ModelLoader()
+            self.log.info(
+                "DocumentIngestor initialized",
+                temp_base=str(self.temp_dir),
+                faiss_base=str(self.faiss_dir),
+                session_id=self.session_id,
+                temp_path=str(self.session_temp_dir),
+                faiss_path=str(self.session_faiss_dir),
+            )
+            
+        except Exception as e:
+            self.log.error("Failed to initialize DocumentIngestor ", error=str(e))
+            raise DocumentPortalException("Initialization error in DocumentIngestor", sys)
     
     def ingest_files(self):
         pass
     
     def _create_retriever(self, documents):
         pass
-    
     
